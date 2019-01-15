093bc67d331ccf221fa6f6110caf4aa4aaf3868c
finfanterodal <finfanterodal@nautilus11.danielcastelao.org>
2019-01-15 09:50:11 +0100

    First Commit License.

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ *Everyone is permitted to copy and distribute verbatim copies
+ *of this license document, but changing it is not allowed.
+ *This version of the GNU Lesser General Public License incorporates
+ *the terms and conditions of version 3 of the GNU General Public
+ *License, supplemented by the additional permissions listed below.
+ */
+package holamundo;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author finfanterodal
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+JOptionPane.showMessageDialog(null,"Hola Mundo");
+    }
+    
+}
